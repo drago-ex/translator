b9896550bf80fae01de1211811f1260c60c434ee
Zdeněk Papučík <zdenek.papucik@gmail.com>
2019-10-08 09:09:36 +0200

    Update bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,12 +2,6 @@
 
 declare(strict_types = 1);
 
-// The Nette Tester command-line runner can be
-// invoked through the command: ../vendor/bin/tester .
-if (@!include __DIR__ . '/../vendor/autoload.php') {
-	echo 'Install Nette Tester using `composer install`';
-	exit(1);
-}
+require __DIR__ . '/../vendor/autoload.php';
 
 Tester\Environment::setup();
-date_default_timezone_set('Europe/Prague');
