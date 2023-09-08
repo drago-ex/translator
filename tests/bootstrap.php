c14107a813f7b5f4fd4c0c2b16c094654fa3b512
accgit <zdenek.papucik@gmail.com>
2023-09-08 12:42:27 +0200

    Update

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
+use Nette\Bootstrap\Configurator;
+
 if (@!include __DIR__ . '/../vendor/autoload.php') {
 	echo 'Install Nette Tester using `composer install`';
 	exit(1);
@@ -11,13 +13,13 @@ if (@!include __DIR__ . '/../vendor/autoload.php') {
 Tester\Environment::setup();
 date_default_timezone_set('Europe/Prague');
 
-const TEMP_DIR = __DIR__ . '/tmp';
+const TemDir = __DIR__ . '/tmp';
 
-@mkdir(dirname(TEMP_DIR));
-@mkdir(TEMP_DIR);
+@mkdir(dirname(TemDir));
+@mkdir(TemDir);
 
-$boot = new Nette\Configurator;
-$boot->setTempDirectory(TEMP_DIR);
+$boot = new Configurator();
+$boot->setTempDirectory(TemDir);
 $boot->createRobotLoader()
 	->addDirectory(__DIR__)
 	->addDirectory(__DIR__ . '/../src')
